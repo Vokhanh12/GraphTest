b6cfb58b255eb34e370da340b5de4747a8a03249
Khánh Nguyễn Võ <khanhyou2018@gmail.com>
2023-03-19 19:09:00 +0700

    add commit

--- src/pa_graph/GraphMain.java
+++ src/pa_graph/GraphMain.java
@@ -5,28 +5,76 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class GraphMain {
 
+public class GraphMain {
 	
+	public static final int MAX = 100;
+	public static final String[] MATRIX_ONLY= {"0","1"};
+	
+	public class GRAPH_MATRIX{
+		private int n;
+		private int[][] Matrix = new int[MAX][MAX];
+		
+		public GRAPH_MATRIX(int n) {
+			this.n=n;
+		}
+		
+		public int nGet() {
+			return n;
+		}
+		
+		public void nSet(int nSet) {
+			this.n=nSet;
+		}
+		
+		public void MatrixSet(int cols,int rows ,int value) {
+			this.Matrix[cols][rows]=value;
+		}
+		
+		public void MatrixGet(int cols,int rows) {
+			
+			for(int i=0;i<cols;i++) {
+				for(int j=0;j<rows;j++) {
+					System.out.print(Matrix[i][j]+" ");
+				}
+				System.out.println("");
+			}
+		}
+		
+		public void KiemTraMaTranHopLe() {
+			int check=0;
+			for(int i=0;i<this.n;i++) {
+				if(Matrix[i][i]!=0)
+				{
+					System.out.println("Ma tran khong hop le");
+					break;
+				} else if(check!=n-1)
+					check++;
+				  else {MatrixGet(n,n);System.out.println("Ma tran nay hop le");}
+			}
+		}
+	}
+	
+	
+	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		GraphMain gm = new GraphMain();
+		GraphMain.GRAPH_MATRIX g = gm.new GRAPH_MATRIX(4);
+		
 		
 		String fileName="D:\\graphfile.txt";
 		try {
 		
-			final FileInputStream fileInput=new FileInputStream(fileName);
-			final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fileInput));
-			
-			
-			InMatrix(bufferedReader);
-			
+			FileInputStream fileInput=new FileInputStream(fileName);
+			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fileInput));
 			
 			
 			//Lấy phần tử đàu tiên
 			int FristLine=Integer.valueOf(bufferedReader.readLine());
 			
 			//Tạo ma trận
-			int Matrix[][] = new int[FristLine][FristLine];
 			
 			//arr Lấy từng hàng và arrGet khởi tạo chuẩn bị tách
 			String[] arr = new String[FristLine];
@@ -43,12 +91,22 @@ public class GraphMain {
 				
 			}
 			
-			for(int i =0 ;i<FristLine;i++) {
+			loop0:for(int i =0 ;i<FristLine;i++) {
 				//arrGet tách
 				arrGet=arr[i].split(" ");
-				for(int j =0 ;j<FristLine;j++) {
-					//Đưa vào matrix
-					Matrix[i][j]=Integer.valueOf(arrGet[j]);
+				loop1:for(int j =0 ;j<FristLine;j++) {
+					
+						if(arrGet[j].equals("0")||arrGet[j].equals("1")) {
+							//Đưa vào matrix
+							g.Matrix[i][j]=Integer.valueOf(arrGet[j]);
+
+						}else {
+							System.out.println("Trong ma tran khong co con so nay:"+arrGet[j]);
+							break loop0;
+						
+					}
+
+					
 				}
 			}
 			
@@ -56,20 +114,23 @@ public class GraphMain {
 			
 			
 			
-			for(int i=0;i<4;i++) {
+	/*		for(int i=0;i<4;i++) {
 				for(int j=0;j<4;j++) {
-					System.out.print(Matrix[i][j]);
+					System.out.print(g.Matrix[i][j]);
 				}
 				System.out.println();
 
 			}
 			
+			*/
 			
+		bufferedReader.close();
+		
 		}catch(IOException e) {
 			e.printStackTrace();
 		}
 		
-		
+		g.KiemTraMaTranHopLe();
 	}
 	
 	
